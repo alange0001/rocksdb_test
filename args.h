7c15951b782da5ce04713a2e9517ae3c743971ae
Adriano Lange <alange0001@gmail.com>
2021-07-01 10:02:09 -0400

    Using container image alange0001/rocksdb_test:4 for version 1.13

--- args.h
+++ args.h
@@ -85,7 +85,7 @@ using std::function;
 		true,                                                     \
 		nullptr)                                                  \
 	_f(docker_image, string, DEFINE_string,                       \
-		"alange0001/rocksdb_test:3",                              \
+		"alange0001/rocksdb_test:4",                              \
 		"docker image used for each container",                   \
 		value.length() > 0,                                       \
 		nullptr)                                                  \
